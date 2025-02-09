2308482919528a8aca03ccc7041d7b92081631f1
zablon <zablonoigo51@gmail.com>
2025-02-09 18:17:51 +0300

    Update ami

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_key_pair" "test_key" {
 }
 
 resource "aws_instance" "web-server" {
-  ami             = var.ami
+  ami             = "ami-01cc34ab2709337aa"
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.test_key.key_name
   security_groups = ["${aws_security_group.web-server.name}"]
